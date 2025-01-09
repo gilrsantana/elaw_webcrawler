09c1bb345e56598d04e2b21d4438dee603994389
gilrsantana <g.r.santana@hotmail.com>
2025-01-09 03:53:55 -0300

    Arquivos de Migration

---
+++ Server/ElawWebCrawler.Data/Migrations/20250109054459_initial_migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElawWebCrawler.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GetDataEvents",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", maxLength: 36, nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    PagesCount = table.Column<int>(type: "int", nullable: false),
+                    RowsCount = table.Column<int>(type: "int", nullable: false),
+                    JsonFile = table.Column<string>(type: "varchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetDataEvents", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GetDataEvents");
+        }
+    }
+}
